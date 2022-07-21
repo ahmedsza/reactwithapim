3d0dfb94e1b82a208f434de9a15fcd2309afcac3
ahmeds <ahmeds@ms.com>
2022-07-21 15:06:43 +0200

    added post

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,11 +6,24 @@ function sendVote() {
     .then(json => alert(json.text));
 }
 
+function sendVotePOST() {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title: 'React POST Request Example' })
+};
+  fetch('/api/votepost',requestOptions)
+    .then(response => response.json())
+    .then(json => alert(json.text));
+}
+
 function App() {
   return (
     <div className="App">
       <button onClick={sendVote}>Vote</button>
+      <button onClick={sendVote}>VotePOST</button>
     </div>
+    
   );
 }
 
