9104b089a0d6409513cc3ec60209e43cb564bee6
Ahmed Salijee <ahmeds@microsoft.com>
2022-07-26 13:41:41 +0200

    updated with no encode

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -17,11 +17,24 @@ function sendVotePOST() {
     .then(json => alert(json.text));
 }
 
+function sendVotePOST2() {
+  const test="{ title: 'React POST Request Example' }";
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: test
+};
+  fetch('/api/votepost',requestOptions)
+    .then(response => response.json())
+    .then(json => alert(json.text));
+}
+
 function App() {
   return (
     <div className="App">
       <button onClick={sendVote}>Vote</button>
       <button onClick={sendVotePOST}>VotePOST</button>
+      <button onClick={sendVotePOST2}>VotePOSTNoEncode</button>
     </div>
     
   );
